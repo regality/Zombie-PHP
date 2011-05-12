50e944701d982e8505bc494c9556de989bdefd3a
Adam Blackburn <regality@gmail.com>
2011-05-11 21:18:54 -0600

    got memcache sessions working

--- brainz/session/memcache_session.php
+++ brainz/session/memcache_session.php
@@ -2,32 +2,53 @@
 require("session.php");
 
 class MemcacheSession extends Session {
-   private $session;
-   private $session_id;
-   private $mem;
+   public static $instance;
+
+   public $session;
+   public $session_id;
+   public $mem;
 
    public function __construct() {
       $this->session = false;
       $this->mem = memcache_connect('localhost', 11211);
       if (isset($_COOKIE['sessid'])) {
          $this->session_id = $_COOKIE['sessid'];
-         $session = memcache_get($this->mem, 'sess_' . $this->session_id);
+         $this->session = memcache_get($this->mem, 'sess_' . $this->session_id);
       }
-      if (!$session) {
+      if ($this->session == false) {
          $this->create();
       } else {
-         $this->session = unserialize($session);
+         $this->session = unserialize($this->session);
          if (!is_array($this->session)) {
             $this->session = array();
          }
       }
    }
 
+   public function __destruct() {
+      if (!isset($this->deleted)) {
+         $this->save();
+      }
+   }
+
+   public static function get_session() {
+      if (!isset(MemcacheSession::$instance)) {
+         MemcacheSession::$instance = new MemcacheSession();
+      }
+      return MemcacheSession::$instance;
+   }
+
+   public function get_array() {
+      return $this->session;
+   }
+
    public function save() {
       $session = serialize($this->session);
+      //memcache_set($this->mem, 'foo', 'bar', 0, 1800);
       memcache_set($this->mem,
                    'sess_' . $this->session_id,
                    $session,
+                   0,
                    1800);
    }
 
@@ -37,10 +58,14 @@ class MemcacheSession extends Session {
                 $this->session_id,
                 time() + 60*30,
                 '/',
-                'wb.regaltic.com',
+                $_SERVER['SERVER_NAME'],
                 false,
                 true);
       $this->session = array();
+   }
+
+   public function is_set($key) {
+      return isset($this->session[$key]);
    }
 
    public function get($key) {
@@ -57,6 +82,7 @@ class MemcacheSession extends Session {
 
    public function clear() {
       memcache_delete($this->mem, 'sess_' . $this->session_id);
+      $this->deleted = true;
       setcookie('sessid','',time() - 1);
    }
 
