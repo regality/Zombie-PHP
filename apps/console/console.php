be04cc797f1d67b60c9028f7368dcd929eb54748
Adam <regality@gmail.com>
2011-08-24 23:24:38 -0600

    added console app in case it is loaded as default

---
+++ apps/console/console.php
@@ -0,0 +1,9 @@
+<?php
+require_once(__DIR__ . "/../../brainz/app/app.php");
+
+class Console extends App {
+   public function index_run($request) {
+   }
+}
+
+?>
---
+++ apps/console/views/index.php
@@ -0,0 +1,5 @@
+<div>
+   <h2>Console</h2><a href="javascript:void(0)" id="console-clear">Clear all</a>
+   <div id="console-messages">
+   </div>
+</div>
