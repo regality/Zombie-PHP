c88ac3eae0a92ffc84d0e02e10d69b2dd17d57a9
Adam <regality@gmail.com>
2011-09-01 03:04:00 -0600

    fixed menu focus on login bug

--- web/js/main.js
+++ web/js/main.js
@@ -58,15 +58,19 @@ $(document).ready(function () {
 
    $("#login-form").live('submit', function () {
       $.ajax({"data" : {"app" : "auth",
-                      "username" : $("#username").val(),
-                      "password" : undead.crypt.hash($("#password").val())},
+                        "username" : $("#username").val(),
+                        "password" : undead.crypt.hash($("#password").val())},
               "success" : function (data) {
                   if (data.status === "success") {
+                     if (typeof data.app === "undefined") {
+                        //data.app = "welcome";
+                     }
                      undead.stack.push(data.app);
                      $.ajax({"data" : {"app" : "menu"},
                              "dataType" : "html",
                              "success" : function (data) {
                                  $("#sidenav").html(data);
+                                 $(".item[href^='/" + undead.stack.activeName() + "']").addClass("active");
                              }
                      });
                   } else {
