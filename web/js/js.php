7a08bcbcc10527ef391434d99db045059a0167bb
Adam <regality@gmail.com>
2011-10-04 23:32:44 -0600

    moved tinymce to build dir

--- web/js/js.php
+++ web/js/js.php
@@ -6,7 +6,7 @@ if (isset($_GET['app']) && isset($_GET['js'])) {
    $js = preg_replace('/[^0-9a-zA-Z_\/\.]/', '', $_GET['js']);
    $js_file = __DIR__ . "/../../apps/" . $app . "/views/scripts/" . $js . ".js";
    if (file_exists($js_file)) {
-      include($js_file);
+      echo file_get_contents($js_file);
    } else {
       header("HTTP/1.1 404 Not Found");
    }
