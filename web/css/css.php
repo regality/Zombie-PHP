f23701dbca0bd525184ed5882e33065044fd5a30
Adam <regality@gmail.com>
2011-10-19 22:47:01 -0600

    added css config options

--- web/css/css.php
+++ web/css/css.php
@@ -1,31 +1,23 @@
 <?php
 
 require_once(__DIR__ . '/../../zombie-core/util/dir.php');
-require_once(__DIR__ . '/../../zombie-core/util/compile/ssp.php');
+require_once(__DIR__ . '/../../zombie-core/util/compile/compile.php');
 
 header("Content-type: text/css");
-if (isset($_GET['type'])) {
-   $app_dir = __DIR__ . '/../../apps/';
-   $apps = get_dir_contents($app_dir);
-   foreach ($apps as $app) {
-      $css_file = __DIR__ . "/../../apps/" . $app . "/views/css/main.css";
-      if ($_GET['type'] == "mobile") {
-         $mobile_css_file = __DIR__ . "/../../apps/" . $app . "/views/css/mobile-main.css";
-         if (file_exists($mobile_css_file)) {
-            $css_file = $mobile_css_file;
-         }
-      }
-      if (file_exists($css_file)) {
-         $css_content = file_get_contents($css_file);
-         $c = new CssFile($css_content);
-         if (isset($_GET['min'])) {
-            echo $c->render(true);
-         } else {
-            echo "\n/* including " . realpath($css_file) . " */\n\n";
-            echo $c->render();
-         }
-      }
-   }
+if (!isset($_GET['name'])) {
+   header("Status: 404 Not Found");
+   exit();
+} else {
+   $name = $_GET['name'];
+}
+
+$file_lists = get_css_file_lists();
+if (!isset($file_lists[$name])) {
+   header("Status: 404 Not Found");
+   exit();
+} else {
+   $output = compile_css_list($file_lists[$name]);
+   echo $output;
 }
 
 ?>
